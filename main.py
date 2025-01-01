41d2bd01771dc7799e6c67e4bf1f58e9c7dbeddf
Ian Williamson <ianwill2014@gmail.com>
2025-01-01 08:06:54 -0600

    Cell draw_move completed)

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from window import Window
+from window import *
+from cell import Cell
 
 def main():
     win = Window(800, 600)
+
+    cell_1 = Cell(win)
+    cell_2 = Cell(win)
+    cell_3 = Cell(win)
+
+    cell_1.has_top_wall = False
+    cell_1.has_bottom_wall = False
+    cell_2.has_top_wall = False
+    cell_2.has_right_wall = False
+    cell_3.has_bottom_wall = False
+    cell_2.has_left_wall = False
+
+    cell_1.draw(50, 50, 150, 150)
+    cell_2.draw(50, 150, 150, 250)
+    cell_3.draw(50, 250, 150, 350)
+
+    cell_1.draw_move(cell_2)
+
     win.wait_for_close()
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -2,26 +2,40 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.running = False
-        
-        self.root = Tk()
-        self.root.title('Main Window')
+        self.__root = Tk()
+        self.__root.title('Main Window')
 
-        self.canvas = Canvas(width=self.width, height=self.height)
-        self.canvas.pack()
+        self.__canvas = Canvas(self.__root, height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
 
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__running = False
+
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running is True:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
+    
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2)
